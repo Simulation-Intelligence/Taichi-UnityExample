55b1c4fa8bdf079f6ae7db64b7db18a6a1db5e06
zhitongcui <2370343775@qq.com>
2024-08-07 21:58:10 +0800

    improve UI, still need to add interface

--- Assets/Utils/UIManager.cs
+++ Assets/Utils/UIManager.cs
@@ -6,16 +6,31 @@ using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
-using System.Threading.Tasks;
 
 class UIManager : MonoBehaviour
 {
-    public Button[] buttons;
-    public TMP_Dropdown[] dropdowns;
-    public InputField[] inputFields;
-
-    public TMP_InputField[] tmpInputFields;
-
+    public GameObject UI_canvas;
+    public Transform UI_anchor;
+    private Vector3 canvas_anchor_offset;
+    [SerializeField]
+    private Camera sceneCamera;
+    [SerializeField]
+    private OVRHand[] oculus_hands;
+    [SerializeField]
+    private OVRSkeleton[] oculus_skeletons;
+    [SerializeField]
+    private Button[] buttons;
+    [SerializeField]
+    private Toggle[] toggles;
+    [SerializeField]
+    private TMP_Dropdown[] dropdowns;
+    [SerializeField]
+    private InputField[] inputFields;
+    [SerializeField]
+    private TMP_InputField[] tmpInputFields;
+    [SerializeField]
+    public GameObject[] privateparameterObjects;
+    
     private TouchScreenKeyboard overlayKeyboard;
     public static string inputText = "";
 
@@ -24,6 +39,11 @@ class UIManager : MonoBehaviour
         foreach (Button button in buttons)
         {
             button.onClick.AddListener(() => OnButtonClick(button));
+        }
+
+        foreach (Toggle toggle in toggles)
+        {
+            toggle.onValueChanged.AddListener((bool isOn) => OnToggleValueChanged(toggle, isOn));
         }
         
         foreach (TMP_Dropdown dropdown in dropdowns)
@@ -40,6 +60,8 @@ class UIManager : MonoBehaviour
         {
            tmpInputField.onSelect.AddListener((string value) => OnTMPInputFieldSelect(tmpInputField));
         }
+
+        canvas_anchor_offset = UI_canvas.transform.position - UI_anchor.position;
     }
 
     void Update()
@@ -67,6 +89,11 @@ class UIManager : MonoBehaviour
         }
     }
 
+    void OnToggleValueChanged(Toggle toggle, bool isOn)
+    {
+        Debug.Log("Toggle " + toggle.name + " is " + (isOn ? "On" : "Off"));
+    }
+
     void OnDropdownValueChanged(TMP_Dropdown dropdown, int value)
     {
 
@@ -77,5 +104,40 @@ class UIManager : MonoBehaviour
     {
         Debug.Log(inputField.name + " was selected!");
         overlayKeyboard = TouchScreenKeyboard.Open(inputField.text, TouchScreenKeyboardType.Default);
+    }
+
+    public void HideCanvas()
+    {
+        if (UI_canvas != null)
+        {
+            UI_canvas.SetActive(false);
+        }
+    }
+    
+    public void ShowCanvas()
+    {
+        if (UI_canvas != null && oculus_hands[1].IsTracked && sceneCamera != null)
+        {
+            Vector3 handThumbTipPosition = Vector3.zero;
+
+            foreach (var b in oculus_skeletons[1].Bones)
+            {
+                if (b.Id == OVRSkeleton.BoneId.Hand_ThumbTip)
+                {
+                    handThumbTipPosition = b.Transform.position;
+                    break;
+                }
+            }
+            UI_canvas.SetActive(true);
+            UI_anchor.position = handThumbTipPosition + sceneCamera.transform.forward * 0.2f;
+            UI_anchor.rotation = Quaternion.LookRotation(sceneCamera.transform.forward);
+            UI_canvas.transform.position = UI_anchor.position + canvas_anchor_offset;
+            UI_canvas.transform.rotation = UI_anchor.rotation;
+        }
+    }
+
+    void OnDestroy()
+    {
+        
     }
 }
