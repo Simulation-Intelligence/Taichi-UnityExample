5d564d0008b811f32f882c61498422ac5325ad68
Roushelfy <2100012985@stu.pku.edu.cn>
2024-10-10 20:03:32 +0800

    scissors

--- Assets/Scripts/MpmTools/MatScissors.cs
+++ Assets/Scripts/MpmTools/MatScissors.cs
@@ -20,7 +20,7 @@ public class MatScissors : MatTool
 
     private Vector3 sphere11 = new Vector3(0, 0, 0);
     private float radii11 = 0.04f;
-    private Vector3 sphere12 = new Vector3(0, 0, 1.0f);
+    private Vector3 sphere12 = new Vector3(1.0f, 0, 0.0f);
     private float radii12 = 0.04f;
     // Note: Set sphere3 and radii3 to 0.0f to treat it as a cone in the system
     private Vector3 sphere13 = new Vector3(0, 0, 0);
@@ -28,7 +28,7 @@ public class MatScissors : MatTool
 
     private Vector3 sphere21 = new Vector3(0, 0, 0);
     private float radii21 = 0.04f;
-    private Vector3 sphere22 = new Vector3(0, 0, 1.0f);
+    private Vector3 sphere22 = new Vector3(1.0f, 0, 0.0f);
     private float radii22 = 0.04f;
     // Note: Set sphere3 and radii3 to 0.0f to treat it as a cone in the system
     private Vector3 sphere23 = new Vector3(0, 0, 0);
@@ -75,30 +75,60 @@ public class MatScissors : MatTool
         // Update mat hand without hand tracking
         if (oculus_hand.IsTracked)
         {
+            Transform transform1 = new GameObject().transform;
+            Transform transform2 = new GameObject().transform;
+            var jointId1 = _handJointId1.ToString().Replace("Hand", "Hand_");
+            var jointId2 = _handJointId2.ToString().Replace("Hand", "Hand_");
             foreach (var bone in oculus_skeleton.Bones)
             {
-                var jointId1 = _handJointId1.ToString().Replace("Hand", "Hand_");
+
                 if (bone.Id == (OVRSkeleton.BoneId)Enum.Parse(typeof(OVRSkeleton.BoneId), jointId1))
                 {
-                    // Rotate 90 degrees around the x-axis to align with the hand joint
-                    transform.position = bone.Transform.position;
-                    transform.rotation = bone.Transform.rotation * _rotationOffset;
 
-                    for (int i = 0; i < numPrimitives; i++)
-                    {
-                        // Update primitive position using the oculus Hand Joint Component
-                        primitives[i].sphere1 = transform.TransformPoint(init_primitives[i].sphere1);
-                        primitives[i].sphere2 = transform.TransformPoint(init_primitives[i].sphere2);
-                        primitives[i].sphere3 = transform.TransformPoint(init_primitives[i].sphere3);
-
-                        // Multiply by localScale to get the correct radius
-                        primitives[i].radii1 = init_primitives[i].radii1 * transform.localScale.x;
-                        primitives[i].radii2 = init_primitives[i].radii2 * transform.localScale.x;
-                        primitives[i].radii3 = init_primitives[i].radii3 * transform.localScale.x;
-                    }
-                    break;
+                    transform1.position = bone.Transform.position;
+                    transform1.rotation = bone.Transform.rotation;
+                    transform1.localScale = transform.localScale;
+                    //
                 }
+                if (bone.Id == (OVRSkeleton.BoneId)Enum.Parse(typeof(OVRSkeleton.BoneId), jointId2))
+                {
+
+                    transform2.position = bone.Transform.position;
+                    transform2.rotation = bone.Transform.rotation;
+                    transform2.localScale = transform.localScale;
+
+                }
+
             }
+
+            Vector3 point1 = transform1.position;
+            Vector3 point2 = transform2.position;
+            Vector3 p12 = point2 - point1;
+            Vector3 normal = Vector3.Cross(transform.right, p12);
+            Vector3 perpendicular = Vector3.Cross(normal, p12);
+            Vector3 midPoint = (point1 + point2) / 2;
+            float len = p12.magnitude / 2;
+            float new_len = Mathf.Sqrt(transform.localScale.x * transform.localScale.x / 9 - len * len);
+            Vector3 cross_point = midPoint + perpendicular.normalized * new_len;
+            {
+                Vector3 xAxis = cross_point - point1;
+                Vector3 zAxis = normal;
+                Vector3 yAxis = Vector3.Cross(zAxis, xAxis).normalized;
+                zAxis = Vector3.Cross(xAxis, yAxis).normalized;  // 确保 Z 轴正交
+                Quaternion targetRotation = Quaternion.LookRotation(zAxis, yAxis);
+                transform1.rotation = targetRotation;
+                UpdatePrimitive(ref primitives[0], init_primitives[0], transform1);
+            }
+            {
+                Vector3 xAxis = cross_point - point2;
+                Vector3 zAxis = normal;
+                Vector3 yAxis = Vector3.Cross(zAxis, xAxis).normalized;
+                zAxis = Vector3.Cross(xAxis, yAxis).normalized;  // 确保 Z 轴正交
+                Quaternion targetRotation = Quaternion.LookRotation(zAxis, yAxis);
+                transform2.rotation = targetRotation;
+                UpdatePrimitive(ref primitives[1], init_primitives[1], transform2);
+            }
+
         }
     }
 }
