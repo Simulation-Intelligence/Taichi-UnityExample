70bd9f1cfecdb419fad36ece0055d6201ada86d2
Roushelfy <2100012985@stu.pku.edu.cn>
2024-10-10 20:42:00 +0800

    scissors

--- Assets/Scripts/MpmTools/MatToolRenderer.cs
+++ Assets/Scripts/MpmTools/MatToolRenderer.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
 [RequireComponent(typeof(MatTool))]
-public class MatToolRenderer: MonoBehaviour
+public class MatToolRenderer : MonoBehaviour
 {
     public MatTool matTool; // Reference to the MatTool component
     public Material material; // Material used for rendering
 
     // Array to store the primitive assemblies (each consisting of three spheres)
     private GameObject[] primitiveObjects;
-    
+
     void Start()
     {
         // Inherit material
@@ -25,26 +25,38 @@ public class MatToolRenderer: MonoBehaviour
 
         // Initialize the primitive assembly array
         primitiveObjects = new GameObject[matTool.numPrimitives];
-        
+
         // Create child objects for each primitive
         for (int i = 0; i < matTool.numPrimitives; i++)
         {
             GameObject primitive = new GameObject("Primitive_" + i);
             primitive.transform.SetParent(transform); // Set child gameObject
             primitive.transform.localScale = Vector3.one;
-            
+
             // Create three spheres
             GameObject sphere1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere1.transform.SetParent(primitive.transform);
-            sphere1.GetComponent<Renderer>().material = material; 
+            sphere1.GetComponent<Renderer>().material = material;
 
             GameObject sphere2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere2.transform.SetParent(primitive.transform);
-            sphere2.GetComponent<Renderer>().material = material; 
+            sphere2.GetComponent<Renderer>().material = material;
 
             GameObject sphere3 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere3.transform.SetParent(primitive.transform);
             sphere3.GetComponent<Renderer>().material = material;
+
+            GameObject cylinder1 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder1.transform.SetParent(primitive.transform);
+            cylinder1.GetComponent<Renderer>().material = material;
+
+            GameObject cylinder2 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder2.transform.SetParent(primitive.transform);
+            cylinder2.GetComponent<Renderer>().material = material;
+
+            GameObject cylinder3 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder3.transform.SetParent(primitive.transform);
+            cylinder3.GetComponent<Renderer>().material = material;
 
             primitive.transform.GetChild(0).localPosition = matTool.init_primitives[i].sphere1;
             primitive.transform.GetChild(1).localPosition = matTool.init_primitives[i].sphere2;
@@ -53,10 +65,12 @@ public class MatToolRenderer: MonoBehaviour
             primitive.transform.GetChild(1).localScale = Vector3.one * matTool.init_primitives[i].radii2 * 2;
             primitive.transform.GetChild(2).localScale = Vector3.one * matTool.init_primitives[i].radii3 * 2;
 
+
+
             primitiveObjects[i] = primitive;
         }
     }
-    
+
     void Update()
     {
         if (matTool == null || primitiveObjects == null)
@@ -77,5 +91,35 @@ public class MatToolRenderer: MonoBehaviour
         primitiveObject.transform.GetChild(0).position = sphere1;
         primitiveObject.transform.GetChild(1).position = sphere2;
         primitiveObject.transform.GetChild(2).position = sphere3;
+
+        Transform cylinder1 = primitiveObject.transform.GetChild(3);
+        Transform cylinder2 = primitiveObject.transform.GetChild(4);
+        Transform cylinder3 = primitiveObject.transform.GetChild(5);
+
+        // Calculate the midpoint and direction
+        Vector3 center1 = (sphere1 + sphere2) / 2.0f;
+        Vector3 direction1 = (sphere2 - sphere1).normalized;
+        float height1 = (sphere2 - sphere1).magnitude;
+
+        Vector3 center2 = (sphere2 + sphere3) / 2.0f;
+        Vector3 direction2 = (sphere3 - sphere2).normalized;
+        float height2 = (sphere3 - sphere2).magnitude;
+
+        Vector3 center3 = (sphere3 + sphere1) / 2.0f;
+        Vector3 direction3 = (sphere1 - sphere3).normalized;
+        float height3 = (sphere1 - sphere3).magnitude;
+
+        // Update the cylinder position and size
+        cylinder1.position = center1;
+        cylinder1.up = direction1;
+        cylinder1.localScale = new Vector3(radii1 * 2, height1 / 2, radii1 * 2) / transform.localScale.x;
+
+        cylinder2.position = center2;
+        cylinder2.up = direction2;
+        cylinder2.localScale = new Vector3(radii2 * 2, height2 / 2, radii2 * 2) / transform.localScale.x;
+
+        cylinder3.position = center3;
+        cylinder3.up = direction3;
+        cylinder3.localScale = new Vector3(radii3 * 2, height3 / 2, radii3 * 2) / transform.localScale.x;
     }
 }
